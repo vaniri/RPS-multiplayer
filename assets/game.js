e06a1435b71a373f45cf58d7d6cef1875fcf3614
Vaniri <foxifennec@gmail.com>
2019-12-12 16:10:01 -0500

    add store score method

--- assets/game.js
+++ assets/game.js
@@ -1,3 +1,8 @@
+
+const WIN = 1;
+const LOSE = 2;
+const DRAW = 0;
+
 const firebaseConfig = {
     apiKey: "AIzaSyC-LjUca_JPCrvoEIn_WkDFeJ6n2ocaWho",
     authDomain: "rps-game-1.firebaseapp.com",
@@ -13,13 +18,12 @@ const db = firebase.firestore();
 
 $("#submit").on("click", event => {
     event.preventDefault();
-    $("body").css('backgroundImage','url("")');
+    $("body").css('backgroundImage', 'linear-gradient(to right, navy, aqua, #94A5AD');
     $("#form-container").hide();
-    $("#image-container").css('visibility', 'visible');
-    $("#user-interaction").css('visibility', 'visible');
+    $("#game-container").css('visibility', 'visible');
     $("#user-interaction").text("Make your choice!");
     db.collection("players").doc($("#nickname").val()).set({
-        win: 0,
+        won: 0,
         lose: 0
     }).catch(function (error) {
         console.error("Error adding document: ", error);
@@ -28,11 +32,13 @@ $("#submit").on("click", event => {
 
 $(".choice-image").each(function () {
     $(this).on("click", () => {
-        let choice = $(this).attr('data-item');
+        choice = $(this).attr('data-item');
         $("#user-interaction").text(`You chose: ${choice}`);
         storeChoice(choice);
-    })
-});
+        //$(".choice-image:nover").removeAttr("style");
+        $(".choice-image").each(function () { $(this).off() });
+    });
+})
 
 const roundRef = db.collection("rounds").doc("round");
 
@@ -71,23 +77,22 @@ roundRef.onSnapshot(snapshot => {
 });
 
 const possibleChoices = ["rock", "paper", "scissors"];
-
 const gameTable = createTable();
 
 function createTable() {
     let table = {};
     table["scissors"] = {};
-    table["scissors"]["scissors"] = 0;
-    table["scissors"]["rock"] = 2;
-    table["scissors"]["paper"] = 1;
+    table["scissors"]["scissors"] = DRAW;
+    table["scissors"]["rock"] = LOSE;
+    table["scissors"]["paper"] = WIN;
     table["rock"] = {};
-    table["rock"]["scissors"] = 1;
-    table["rock"]["rock"] = 0;
-    table["rock"]["paper"] = 2;
+    table["rock"]["scissors"] = WIN;
+    table["rock"]["rock"] = DRAW;
+    table["rock"]["paper"] = LOSE;
     table["paper"] = {};
-    table["paper"]["scissors"] = 2;
-    table["paper"]["rock"] = 1;
-    table["paper"]["paper"] = 0;
+    table["paper"]["scissors"] = LOSE;
+    table["paper"]["rock"] = WIN;
+    table["paper"]["paper"] = DRAW;
     return table;
 }
 
@@ -101,14 +106,15 @@ function gameLogic(myChoice, otherUserChoice) {
     console.log(result);
 }
 
-
 function showMeResult(result) {
-    const showResult = $('<h1 id="show-result"></h1>');
+    let showResult = $('<h1 id="show-result"></h1>');
     console.log(showResult);
-    $("#user-interaction").append.showResult;
-    if (result === 1) {
+    $("#user-interaction").append(showResult);
+    if (result === WIN) {
+        db.collection("players").doc($("#nickname").val()).update({won:firebase.firestore.FieldValue.increment(1)});
         showResult.text("You won!");
-    } else if (result === 2) {
-        showResult.text("You lose!");
+    } else if (result === LOSE) {
+        db.collection("players").doc($("#nickname").val()).update({lose:firebase.firestore.FieldValue.increment(1)});
+        showResult.text("You lost !");
     } else { showResult.text("Draw!") };
 } 
